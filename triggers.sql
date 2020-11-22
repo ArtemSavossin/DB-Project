e16ff7f5296999d1a8ddc19134e4a8b8039e8ca8
Artyom Savossin <46690976+ArtemSavossin@users.noreply.github.com>
2020-11-22 14:31:56 +0600

    triggers for db

---
+++ triggers.sql
@@ -0,0 +1,103 @@
+
+CREATE OR REPLACE FUNCTION order_cost_count_trg()
+RETURNS trigger AS
+$$
+BEGIN
+	UPDATE orders SET total_cost = (SELECT SUM(cost  * amount) FROM Order_item JOIN Items ON Items.id_item = Order_item.id_item 
+	WHERE order_item.id_order = NEW.id_order) WHERE orders.id_order = new.id_order;
+  RETURN NEW;
+END;
+$$
+LANGUAGE 'plpgsql';
+
+CREATE TRIGGER order_cost_count
+  AFTER INSERT
+  ON "order_item"
+  FOR EACH ROW
+  EXECUTE PROCEDURE order_cost_count_trg();
+
+CREATE OR REPLACE FUNCTION order_cost_count_del_trg()
+RETURNS trigger AS
+$$
+BEGIN
+	UPDATE orders SET total_cost = (SELECT coalesce(SUM(cost  * amount),0) FROM Order_item JOIN Items ON Items.id_item = Order_item.id_item 
+  WHERE order_item.id_order = old.id_order) WHERE orders.id_order = old.id_order;
+  RETURN OLD;
+END;
+$$
+LANGUAGE 'plpgsql';
+
+CREATE TRIGGER order_cost_count_del
+  AFTER DELETE
+  ON "order_item"
+  FOR EACH ROW
+  EXECUTE PROCEDURE order_cost_count_del_trg();
+
+CREATE OR REPLACE FUNCTION order_cost_count_upd_trg()
+RETURNS trigger AS
+$$
+BEGIN
+	UPDATE orders SET total_cost = (SELECT SUM(cost  * amount) FROM Order_item JOIN Items ON Items.id_item = Order_item.id_item 
+	WHERE order_item.id_order = NEW.id_order) WHERE orders.id_order = new.id_order;
+  RETURN NEW;
+END;
+$$
+LANGUAGE 'plpgsql';
+
+CREATE TRIGGER order_cost_count_upd
+  AFTER UPDATE
+  ON "order_item"
+  FOR EACH ROW
+  EXECUTE PROCEDURE order_cost_count_upd_trg();
+
+CREATE OR REPLACE FUNCTION storage_place_count_trg()
+RETURNS trigger AS
+$$
+BEGIN
+	UPDATE Storages SET avialable_place = (SELECT capacity - SUM(amount) FROM storage_item JOIN Items ON Items.id_item = storage_item.id_item 
+  WHERE storage_item.id_storage = NEW.id_storage) WHERE Storages.id_storage = new.id_storage;
+  RETURN NEW;
+END;
+$$
+LANGUAGE 'plpgsql';
+ 
+CREATE TRIGGER storage_place_count
+  AFTER INSERT
+  ON "storage_item"
+  FOR EACH ROW
+  EXECUTE PROCEDURE storage_place_count_trg();
+
+CREATE OR REPLACE FUNCTION storage_place_count_upd_trg()
+RETURNS trigger AS
+$$
+BEGIN
+	UPDATE Storages SET avialable_place = (SELECT capacity - SUM(amount) FROM storage_item JOIN Items ON Items.id_item = storage_item.id_item 
+  WHERE storage_item.id_storage = NEW.id_storage) WHERE Storages.id_storage = new.id_storage;
+  RETURN NEW;
+END;
+$$
+LANGUAGE 'plpgsql';
+ 
+CREATE TRIGGER storage_place_count_upd
+  AFTER UPDATE
+  ON "storage_item"
+  FOR EACH ROW
+  EXECUTE PROCEDURE storage_place_count_upd_trg();
+
+CREATE OR REPLACE FUNCTION storage_place_count_del_trg()
+RETURNS trigger AS
+$$
+BEGIN
+	UPDATE Storages SET avialable_place = (SELECT capacity - coalesce(SUM(amount),0) FROM Storage_item JOIN Items ON Items.id_item = Storage_item.id_item 
+  WHERE storage_item.id_storage = old.id_storage) WHERE Storages.id_storage = old.id_storage;
+  RETURN OLD;
+END;
+$$
+LANGUAGE 'plpgsql';
+
+CREATE TRIGGER storage_place_count_del
+  AFTER DELETE
+  ON "storage_item"
+  FOR EACH ROW
+  EXECUTE PROCEDURE storage_place_count_del_trg();
+  
